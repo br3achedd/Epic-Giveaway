5ac8891852d3b2c6ce06fb9428389b6ca0f12124
br3achedd <63490999+br3achedd@users.noreply.github.com>
2025-07-14 20:28:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("win-button").addEventListener("click", function () {
+  alert("GRATULÁLUNK! MEGNYERTE AZ ULTIMATE HAJSZÁRÍTÓT. MOST SPECIÁLBA 3 IN 1, HAJSZÁRÍTÓ+DILDO+PORSZÍVÓ, MIND CSAK EGY KESZÜLÉKBEN.");
+});
